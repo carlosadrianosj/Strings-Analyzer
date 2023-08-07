81213b9c7c5ef763fd726fcab50a28d9c27e2c2c
Carlos Adriano de Souza Jorge <71354599+carlosadrianosj@users.noreply.github.com>
2023-08-07 07:56:34 -0300

    Add files via upload

---
+++ Extract_Compression.py
@@ -0,0 +1,53 @@
+import os
+import zipfile
+import rarfile
+import gzip
+import shutil
+import py7zr
+
+# Função para extrair arquivos zip
+def extract_zip(file_path, output_path):
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(output_path)
+
+# Função para extrair arquivos rar
+def extract_rar(file_path, output_path):
+    with rarfile.RarFile(file_path) as rf:
+        rf.extractall(path=output_path)
+
+# Função para extrair arquivos gzip
+def extract_gzip(file_path, output_path):
+    with gzip.open(file_path, 'rb') as f_in:
+        with open(os.path.join(output_path, os.path.splitext(os.path.basename(file_path))[0]), 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+# Função para extrair arquivos 7z
+def extract_7z(file_path, output_path):
+    with py7zr.SevenZipFile(file_path, mode='r') as z:
+        z.extractall(path=output_path)
+
+# Função para percorrer diretórios e extrair arquivos
+def extract_files_in_directory(search_directory):
+    for dirpath, dirs, files in os.walk(search_directory):
+        for filename in files:
+            file_path = os.path.join(dirpath, filename)
+
+            # Cria um diretório de saída para cada arquivo
+            output_path = os.path.join(dirpath, os.path.splitext(filename)[0])
+            os.makedirs(output_path, exist_ok=True)
+
+            # Verifica o tipo de arquivo e chama a função apropriada para extraí-lo
+            if filename.lower().endswith(".zip"):
+                extract_zip(file_path, output_path)
+            elif filename.lower().endswith(".rar"):
+                extract_rar(file_path, output_path)
+            elif filename.lower().endswith(".gzip"):
+                extract_gzip(file_path, output_path)
+            elif filename.lower().endswith(".7z"):
+                extract_7z(file_path, output_path)
+
+# Caminho do diretório onde a busca será realizada
+search_directory = "/mnt/c/Users/cjorge/Documents/python file search/analise/123/teste"
+
+# Chama a função para extrair arquivos
+extract_files_in_directory(search_directory)
---
+++ Strings_Analyzer.py
@@ -0,0 +1,83 @@
+import os
+import subprocess
+import argparse
+import shlex
+from collections import defaultdict
+import csv
+
+def create_parser():
+    '''
+    Função para criar um ArgumentParser para lidar com argumentos da linha de comando.
+    '''
+    parser = argparse.ArgumentParser(description='Busca strings em arquivos dentro de um diretório.')
+    parser.add_argument('dir', type=str, help='O diretório onde a busca será realizada.')
+    parser.add_argument('-fast', action='store_true', help='Executa a busca usando utilitários do Linux.')
+    return parser
+
+
+def get_target_strings():
+    '''
+    Função para ler o arquivo de strings alvo e retornar uma lista das strings.
+    '''
+    with open('target_strings.txt', 'r') as f:
+        target_strings = [line.strip() for line in f]
+    return target_strings
+
+def execute_command(search_directory, target_string):
+    '''
+    Função para executar o comando grep e retornar a saída.
+    '''
+    command = f"grep -riwoE '{target_string}' {search_directory}"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    out, err = process.communicate()
+    return out
+
+def parse_results(out):
+    '''
+    Função para analisar a saída do comando grep e retornar um dicionário dos resultados.
+    '''
+    lines = out.decode('utf-8').splitlines()
+    results = defaultdict(list)
+    for line in lines:
+        if ':' in line:
+            filepath, match = line.split(':', 1)
+            results[filepath].append(match)
+    return results
+
+def print_results(results):
+    '''
+    Função para imprimir os resultados como uma tabela.
+    '''
+    print(f"{'Arquivo':<150} {'Strings'}")
+    for filepath, matches in results.items():
+        filename = os.path.basename(filepath)
+        print(f"{filename:<150} {matches}")
+
+def save_results(results):
+    '''
+    Função para salvar os resultados em um arquivo CSV.
+    '''
+    with open('results.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Caminho', 'Strings'])
+        for filepath, matches in results.items():
+            writer.writerow([filepath, ', '.join(matches)])
+
+def main():
+    '''
+    Função principal que coordena a execução do script.
+    '''
+    parser = create_parser()
+    args = parser.parse_args()
+    target_strings = get_target_strings()
+    target_string = '|'.join(target_strings)
+    search_directory = shlex.quote(args.dir)
+    
+    if args.fast:
+        out = execute_command(search_directory, target_string)
+        results = parse_results(out)
+        print_results(results)
+        save_results(results)
+
+if __name__ == "__main__":
+    main()
